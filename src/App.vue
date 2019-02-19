4e3fba98f0a5815d1bae2a0c8f5790b915e175d6
Pablo Scasso <pablo@emersonmedia.com>
2019-02-18 23:57:17 -0300

    First initial wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+
+    <header class="header">
+      <h1 class="title">Find the closest venues to you</h1>
+
+      <button class="btn">
+        See venues close to me
+      </button>
+    </header>
+
+    <section class="venues-list">
+      <div class="venue-card">
+        <h3 class="venue-title">The Venue</h3>
+        <p>description</p>
+      </div>
+    </section>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import 'roboto-fontface/css/roboto/sass/roboto-fontface.scss';
+import 'normalize.css';
 import '@/main.scss';
 
 Vue.config.productionTip = false;
